047837264e48a70a6a197264f8b972bdf0ba850f
Uzay Peker <58823370+spacegondar@users.noreply.github.com>
2022-01-07 05:50:12 +0300

    Add files via upload

---
+++ console-programlama/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace console_programlama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("İsminizi Giri");
+            string name =Console.ReadLine();
+
+            Console.WriteLine("Selam " + name);
+        }
+    }
+}
